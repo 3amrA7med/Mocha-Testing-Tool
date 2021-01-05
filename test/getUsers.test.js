cd9a8b91e8757a0597674187916400fb5cc6c26d
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2021-01-05 23:06:56 +0200

    Added markdown results

--- test/getUsers.test.js
+++ test/getUsers.test.js
@@ -30,7 +30,7 @@ describe('Get data from API, test Async function with call back', function() {
 });
 
 describe('Get data from API, Return a promise', function() {
-    this.slow(5000);
+    
 
     it('should return 10 users', function() {
     return getUsers().
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -49,11 +49,11 @@ describe('Testing shapesList', function() {
         assert(shapes.list.avgAreas, 1);
     })
 
-    // Exclusive, only test excuted
-    // it.only('Compute stats failed error', () => {
-    //     shapes.computeStats()
-    //     assert(shapes.list.avgAreas, 1);
-    // })
+    //Exclusive, only test excuted
+    it.only('Compute stats failed error', () => {
+        shapes.computeStats()
+        assert(shapes.list.avgAreas, 1);
+    })
 
     after("Check list elements count after all tests", function() {
         delete shapes;
